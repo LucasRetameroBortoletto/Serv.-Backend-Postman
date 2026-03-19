09a06b57c8489ac44e05086760c27ec1be9f50db
LucasRetameroBortoletto <lrbortoletto@gmail.com>
2026-03-19 10:37:34 -0300

    Serv. Backend/Postman

--- app-get-post.py
+++ app-get-post.py
@@ -1,23 +1,24 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 class servidor(BaseHTTPRequestHandler):
-
+#Metodo GET(usado anteriormente)
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Servidor Backend do claudio Funcionando")
-
+        self.wfile.write(b"Servidor em funcionamento com o GET")
+#Metodo POST(novo)
     def do_POST(self):
-
+        #identifica o tamanho dos arquivos enviados na requisição
         tamanho = int(self.headers['Content-Length']) 
 
+        #le os dados enviados
         dados = self.rfile.read(tamanho)
-
+        
+        #Mostra os dados recebidos do terminal
         print("Dados recebidos: ", dados.decode())
 
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Post do cloud recebido")
+        self.wfile.write(b"POST recebido!") #retorna uma mensagem para o cliente 
 
 HTTPServer(("0.0.0.0", 8000), servidor).serve_forever()
-#comentario apenas para testar o git, porque sim.
