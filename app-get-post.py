41cb0a6aec9285f733a3a9f2682e4a8cdf1ce4b5
LucasRetameroBortoletto <lrbortoletto@gmail.com>
2026-03-19 10:07:22 -0300

    Servidor Backend

---
+++ app-get-post.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+class servidor(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Servidor Backend do claudio Funcionando")
+
+    def do_POST(self):
+
+        tamanho = int(self.headers['Content-Length'])
+
+        dados = self.rfile.read(tamanho)
+
+        print("Dados recebidos: ", dados.decode())
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Post do cloud recebido")
+
+HTTPServer(("0.0.0.0", 8000), servidor).serve_forever()
